115357204df04b1a4abe7f5b387cb2c6021c29c9
Heer1804 <parikhheer8@gmail.com>
2024-12-26 18:01:43 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const db = require("./config/database");
+
+const port = 3000;
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname + "/assets")));
+app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
+
+app.use("/", require("./routers"));
+
+app.listen(port, (err) => {
+  if (!err) {
+    db();
+    console.log("Server started on http://localhost:" + port);
+  }
+});
